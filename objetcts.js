8490500403b7c351fd08e40b58d304756d4b0133
MRX <131702289+hhamidullohhub@users.noreply.github.com>
2024-06-26 15:26:31 +0500

    add new object

--- objetcts.js
+++ objetcts.js
@@ -1,28 +1,28 @@
 const movies = [
     {
-        img: "https://darakchi.uz/storage/86/08/8e/93727/conversions/27337866_PopjLd8_Hn4ql2yut7GzFOizVrZHw18kq5tARK-vbhI-xl.jpg",
-        title: 'xorlangan moykachi',
+        img: "img/deadpool.jpg",
+        title: 'Deadpool',
         year: 2024,
         desc: 'yoqud ijodkor 1999-yil xorazm viloyatida tavallud topadi keyin u xorlangan moykachi bolib qoladi'
 
     },
     {
-        img: "https://darakchi.uz/storage/86/08/8e/93727/conversions/27337866_PopjLd8_Hn4ql2yut7GzFOizVrZHw18kq5tARK-vbhI-xl.jpg",
-        title: 'xorlangan moykachi',
+        img: "img/black panther.jpg",
+        title: 'Black panther 2',
         year: 2024,
         desc: 'yoqud ijodkor 1999-yil xorazm viloyatida tavallud topadi keyin u xorlangan moykachi bolib qoladi'
 
     },
     {
-        img: "https://darakchi.uz/storage/86/08/8e/93727/conversions/27337866_PopjLd8_Hn4ql2yut7GzFOizVrZHw18kq5tARK-vbhI-xl.jpg",
-        title: 'xorlangan moykachi',
+        img: "img/farsaj.jpg",
+        title: 'Farsaj 8',
         year: 2024,
         desc: 'yoqud ijodkor 1999-yil xorazm viloyatida tavallud topadi keyin u xorlangan moykachi bolib qoladi'
 
     },
     {
-        img: "https://darakchi.uz/storage/86/08/8e/93727/conversions/27337866_PopjLd8_Hn4ql2yut7GzFOizVrZHw18kq5tARK-vbhI-xl.jpg",
-        title: 'xorlangan moykachi',
+        img: "img/spiderman.jpeg",
+        title: 'Spiderman',
         year: 2024,
         desc: 'yoqud ijodkor 1999-yil xorazm viloyatida tavallud topadi keyin u xorlangan moykachi bolib qoladi'
 
